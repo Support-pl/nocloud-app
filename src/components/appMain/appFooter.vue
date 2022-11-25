1338624e2d44fd7eb05fef9753e357c4cf3bd7ea
SazukinPavel <sazukinpavelal@gmail.com>
2022-11-25 16:36:51 +0300

    hide other tabs

--- src/components/appMain/appFooter.vue
+++ src/components/appMain/appFooter.vue
@@ -1,125 +1,129 @@
 <template>
-	<div class="footer">
-		<div class="container">
-			<div class="footer__content">
-				<div v-for="(button, index) in getButtons" :key="index" @click="setTabByName(button.title)" class="button" :class="{ active: button.title==active }">
-					<div class="button__icon">
-						<a-icon :type="button.icon" :theme="button.theme" />
-					</div>
-					<div class="button__title">
-						{{$t(button.title)}}
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="footer">
+    <div class="container">
+      <div class="footer__content">
+        <div
+          v-for="(button, index) in getButtons"
+          :key="index"
+          @click="setTabByName(button.title)"
+          class="button"
+          :class="{ active: button.title == active }"
+        >
+          <div class="button__icon">
+            <a-icon :type="button.icon" :theme="button.theme" />
+          </div>
+          <div class="button__title">
+            {{ $t(button.title) }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters } from "vuex";
 export default {
-	name: "appFooter",
-	computed: {
-		...mapGetters('app', ['getButtons']),
-		...mapGetters('app', ['getActiveTab']),
-		active(){
-			const footerTitle = this.$route.meta?.footerTitle;
-			const layoutTitle = this.$route.meta?.layoutTitle;
-			if(footerTitle) return footerTitle;
-			if(layoutTitle) return layoutTitle;
-			return this.getActiveTab.title;
-		}
-	},
-	methods: {
-		...mapActions('app', ['setTabByName'])
-	}
-}
+  name: "appFooter",
+  computed: {
+    ...mapGetters("app", ["getButtons"]),
+    ...mapGetters("app", ["getActiveTab"]),
+    active() {
+      const footerTitle = this.$route.meta?.footerTitle;
+      const layoutTitle = this.$route.meta?.layoutTitle;
+      if (footerTitle) return footerTitle;
+      if (layoutTitle) return layoutTitle;
+      return this.getActiveTab.title;
+    },
+  },
+  methods: {
+    ...mapActions("app", ["setTabByName"]),
+  },
+};
 </script>
 
 <style scoped>
-
-.footer{
-	background-color:	var(--bright_bg);
-	position: relative;
-	box-shadow: 0px 0px 15px rgba(0, 0, 0, .2);
-	user-select: none;
+.footer {
+  background-color: var(--bright_bg);
+  position: relative;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.2);
+  user-select: none;
 }
 
-.footer__content{
-	display: flex;
-	position: relative;
+.footer__content {
+  display: flex;
+  position: relative;
 }
 
-.button{
-	flex: 1 0;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	cursor: pointer;
-	position: relative;
-	z-index: 2;
+.button {
+  flex: 1 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  z-index: 2;
 }
 
-.button__title{
-	transform: translateY(20px);
-	transition: transform .2s .1s ease;
+.button__title {
+  transform: translateY(20px);
+  transition: transform 0.2s 0.1s ease;
 }
 
-.button__icon{
-	font-size: 1.2rem;
-	color: var(--gray);
-	position: relative;
-	z-index: 2;
-	translate: color .2s ease;
-	transform: translateY(10px);
-	transition: transform .2s ease;
+.button__icon {
+  font-size: 1.2rem;
+  color: var(--gray);
+  position: relative;
+  z-index: 2;
+  translate: color 0.2s ease;
+  transform: translateY(10px);
+  transition: transform 0.2s ease;
 }
 
-.button:hover .button__icon{
-	filter: brightness(0.8517);
+.button:hover .button__icon {
+  filter: brightness(0.8517);
 }
-.footer__active{
-	background-color: var(--main);
-	height: 52px;
-	width: 52px;
-	position: absolute;
-	top: -30px;
-	left: 47px;
-	border-radius: 50%;
-	z-index: 2;
-	color: var(--bright_font);
-	transform: translateX(-50%);
-}
-
-.footer__active::before{
-	content: '';
-	display: block;
-	width: 60px;
-	height: 60px;
-	background-color: var(--bright_bg);
-	border-radius: 50%;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%,-50%);
-	z-index: 1;
-	box-shadow: 0px 0px 15px rgba(0, 0, 0, .2);
+.footer__active {
+  background-color: var(--main);
+  height: 52px;
+  width: 52px;
+  position: absolute;
+  top: -30px;
+  left: 47px;
+  border-radius: 50%;
+  z-index: 2;
+  color: var(--bright_font);
+  transform: translateX(-50%);
 }
 
-.button.active{
-	background-color: var(--main);
+.footer__active::before {
+  content: "";
+  display: block;
+  width: 60px;
+  height: 60px;
+  background-color: var(--bright_bg);
+  border-radius: 50%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.2);
 }
 
-
-.button.active .button__icon{
-	color: var(--bright_font);
-	transform: translateY(5px);
+.button.active {
+  background-color: var(--main);
 }
 
-.button.active .button__title{
-	transform: none;
-	color: var(--bright_font);
+.button.active .button__icon {
+  color: var(--bright_font);
+  transform: translateY(5px);
+}
+
+.button.active .button__title {
+  transform: none;
+  color: var(--bright_font);
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,266 +1,278 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import appMain from '../components/appMain/appMain.vue'
-import config from '../appconfig'
-import i18n from '@/i18n'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import appMain from "../components/appMain/appMain.vue";
+import config from "../appconfig";
+import i18n from "@/i18n";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const servicesArray = config.services;
 const services = [];
 for (let service in servicesArray) {
-	const temp = {
-		path: service,
-		name: `service-${service}`,
-		meta: {
-			footerTitle: 'services',
-			isNeedBackButton: true,
-			headerTitle: service,
-			componentName: service,
-			productsGroupName: servicesArray[service].groupname
-		},
-		component: () => import(`../components/services/${service}/index.vue`),
-	}
-	if (config.services[service].additionalRoutes) {
-		for (let additionalRoute of config.services[service].additionalRoutes) {
-			const route = {
-				path: additionalRoute.path,
-				name: additionalRoute.name,
-				meta: additionalRoute.meta,
-				component: () => import(`../components/services/${service}/${additionalRoute.componentName}.vue`)
-			}
-			services.push(route);
-		}
-	}
-	services.push(temp);
+  const temp = {
+    path: service,
+    name: `service-${service}`,
+    meta: {
+      footerTitle: "services",
+      isNeedBackButton: true,
+      headerTitle: service,
+      componentName: service,
+      productsGroupName: servicesArray[service].groupname,
+    },
+    component: () => import(`../components/services/${service}/index.vue`),
+  };
+  if (config.services[service].additionalRoutes) {
+    for (let additionalRoute of config.services[service].additionalRoutes) {
+      const route = {
+        path: additionalRoute.path,
+        name: additionalRoute.name,
+        meta: additionalRoute.meta,
+        component: () =>
+          import(
+            `../components/services/${service}/${additionalRoute.componentName}.vue`
+          ),
+      };
+      services.push(route);
+    }
+  }
+  services.push(temp);
 }
 
 const routes = [
-	{
-		path: '/login',
-		name: 'login',
-		meta: {
-			mustBeLoggined: false,
-			mustBeUnloggined: true,
-		},
-		component: () => import('../routes/login.vue')
-	},
-	{
-		path: '/register',
-		name: 'register',
-		meta: {
-			mustBeLoggined: false,
-			mustBeUnloggined: true,
-		},
-		component: () => import('../routes/register.vue')
-	},
-	{
-		path: '/',
-		component: appMain,
-		children: [
-			{
-				path: '/',
-				name: 'root',
-				meta: {
-					layoutTitle: 'services',
-					mustBeLoggined: true,
-				},
-				component: () => import('../routes/services.vue')
-			},
-			{
-				path: 'cloud',
-				name: 'cloud',
-				meta: {
-					mustBeLoggined: false,
-				},
-				component: () => import('../routes/cloud.vue')
-			},
-			{
-				path: 'services',
-				name: 'services',
-				meta: {
-					// mustBeLoggined: true,
-				},
-				component: () => import('../routes/services.vue'),
-			},
-			...services,
-			{
-				path: 'products',
-				name: 'products',
-				meta: {
-					// mustBeLoggined: true,
-					layoutTitle: 'services',
-					isNeedBackButton: true,
-				},
-				component: () => import('../routes/products.vue')
-			},
-			{
-				path: 'support',
-				name: 'support',
-				meta: {
-					mustBeLoggined: true,
-				},
-				component: () => import('../routes/support.vue')
-			},
-			{
-				path: 'invoice',
-				name: 'invoice',
-				meta: {
-					mustBeLoggined: true,
-				},
-				component: () => import('../routes/invoice.vue')
-			},
-			{
-				path: 'settings',
-				name: 'settings',
-				meta: {
-					mustBeLoggined: true,
-				},
-				component: () => import('../routes/settings.vue')
-			},
-			{
-				path: 'cloud/newPaaS',
-				name: 'newPaaS',
-				meta: {
-					footerTitle: 'cloud',
-					isFromRoute: true,
-				},
-				component: () => import('../components/appMain/newPaaS.vue')
-			},
-			// {
-			// 	path: 'cloud/plans',
-			// 	name: 'newPaaS_new',
-			// 	meta: {
-			// 		footerTitle: 'cloud'
-			// 	},
-			// 	component: () => import('../components/appMain/newPaaS_new.vue')
-			// },
-			{
-				path: 'cloud/newVDC',
-				name: 'newVDC',
-				meta: {
-					// mustBeLoggined: true,
-					footerTitle: 'cloud'
-				},
-				component: () => import('../components/appMain/newVDC.vue')
-			},
-			{
-				path: 'service/:id',
-				name: 'service',
-				meta: {
-					mustBeLoggined: true,
-					footerTitle: 'services',
-					headerTitle: 'service',
-					isNeedBackButton: true,
-				},
-				component: () => import('../routes/userService.vue'),
-			},
-			{
-				path: 'service/:id/certificate',
-				name: 'certificate',
-				meta: {
-					mustBeLoggined: true,
-					headerTitle: i18n.t('ssl.certificate_configuration'),
-					isNeedBackButton: true,
-				},
-				props: true,
-				component: () => import('../routes/ssl/certificate.vue')
-			},
-			{
-				path: 'service/:id/certificate/csr',
-				name: 'csr',
-				meta: {
-					mustBeLoggined: true,
-					headerTitle: 'CSR',
-					isNeedBackButton: true,
-				},
-				component: () => import('../routes/ssl/csr.vue')
-			},
-			{
-				path: 'service/:id/ssl',
-				name: 'ssl',
-				meta: {
-					mustBeLoggined: true,
-					headerTitle: 'SSL certificate',
-					isNeedBackButton: true,
-				},
-				component: () => import('../routes/ssl/ssl.vue')
-			},
-			{
-				path: 'cabinet',
-				name: 'cabinet',
-				meta: {
-					mustBeLoggined: true,
-					footerTitle: 'settings',
-					headerTitle: 'settings',
-					isNeedBackButton: true,
-				},
-				component: () => import('../routes/userSettings.vue')
-			}
-		]
-	},
-	{
-		path: '/cloud/:uuid',
-		name: 'openCloud_new',
-		component: () => import('../components/appMain/cloud/openCloud_new.vue'),
-		meta: {
-			mustBeLoggined: true,
-		},
-	},
-	{
-		path: '/cloud/*',
-		component: () => import('../components/appMain/cloud/cloudRouter.vue'),
-		meta: {
-			mustBeLoggined: true,
-		},
-		children: [
-			{
-				path: 'vnc',
-				name: 'VNC',
-				component: () => import('../components/appMain/cloud/vnc.vue')
-			},
-			{
-				path: '*',
-				redirect: to => `/cloud-${to.params.uuid}`
-			},
-			{
-				path: '',
-				name: 'openCloud',
-				component: () => import('../components/appMain/cloud/openCloud_new.vue')
-			},
-		]
-	},
-	{
-		path: '/ticket-*',
-		name: 'ticket',
-		meta: {
-			mustBeLoggined: true,
-		},
-		component: () => import('../components/appMain/support/ticketchat.vue')
-	},
-	{
-		path: '/invoice/:uuid',
-		name: 'invoiceFS',
-		meta: {
-			mustBeLoggined: true,
-		},
-		component: () => import('../components/appMain/invoice/openInvoice.vue')
-	},
   {
-    path: '/transaction/:uuid',
-    name: 'transaction',
+    path: "/login",
+    name: "login",
+    meta: {
+      mustBeLoggined: false,
+      mustBeUnloggined: true,
+    },
+    component: () => import("../routes/login.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: {
+      mustBeLoggined: false,
+      mustBeUnloggined: true,
+    },
+    component: () => import("../routes/register.vue"),
+  },
+  {
+    path: "/",
+    component: appMain,
+    children: [
+      {
+        path: "/",
+        name: "root",
+        meta: {
+          layoutTitle: "services",
+          mustBeLoggined: true,
+        },
+        component: () => import("../routes/services.vue"),
+      },
+      {
+        path: "cloud",
+        name: "cloud",
+        meta: {
+          mustBeLoggined: false,
+        },
+        component: () => import("../routes/cloud.vue"),
+      },
+      {
+        path: "services",
+        name: "services",
+        meta: {
+          // mustBeLoggined: true,
+        },
+        component: () => import("../routes/services.vue"),
+      },
+      ...services,
+      {
+        path: "products",
+        name: "products",
+        meta: {
+          // mustBeLoggined: true,
+          layoutTitle: "services",
+          isNeedBackButton: true,
+        },
+        component: () => import("../routes/products.vue"),
+      },
+      {
+        path: "support",
+        name: "support",
+        meta: {
+          mustBeLoggined: true,
+        },
+        component: () => import("../routes/support.vue"),
+      },
+      {
+        path: "invoice",
+        name: "invoice",
+        meta: {
+          mustBeLoggined: true,
+        },
+        component: () => import("../routes/invoice.vue"),
+      },
+      {
+        path: "settings",
+        name: "settings",
+        meta: {
+          mustBeLoggined: true,
+        },
+        component: () => import("../routes/settings.vue"),
+      },
+      {
+        path: "cloud/newPaaS",
+        name: "newPaaS",
+        meta: {
+          footerTitle: "cloud",
+          isFromRoute: true,
+        },
+        component: () => import("../components/appMain/newPaaS.vue"),
+      },
+      // {
+      // 	path: 'cloud/plans',
+      // 	name: 'newPaaS_new',
+      // 	meta: {
+      // 		footerTitle: 'cloud'
+      // 	},
+      // 	component: () => import('../components/appMain/newPaaS_new.vue')
+      // },
+      {
+        path: "cloud/newVDC",
+        name: "newVDC",
+        meta: {
+          // mustBeLoggined: true,
+          footerTitle: "cloud",
+        },
+        component: () => import("../components/appMain/newVDC.vue"),
+      },
+      {
+        path: "service/:id",
+        name: "service",
+        meta: {
+          mustBeLoggined: true,
+          footerTitle: "services",
+          headerTitle: "service",
+          isNeedBackButton: true,
+        },
+        component: () => import("../routes/userService.vue"),
+      },
+      {
+        path: "service/:id/certificate",
+        name: "certificate",
+        meta: {
+          mustBeLoggined: true,
+          headerTitle: i18n.t("ssl.certificate_configuration"),
+          isNeedBackButton: true,
+        },
+        props: true,
+        component: () => import("../routes/ssl/certificate.vue"),
+      },
+      {
+        path: "service/:id/certificate/csr",
+        name: "csr",
+        meta: {
+          mustBeLoggined: true,
+          headerTitle: "CSR",
+          isNeedBackButton: true,
+        },
+        component: () => import("../routes/ssl/csr.vue"),
+      },
+      {
+        path: "service/:id/ssl",
+        name: "ssl",
+        meta: {
+          mustBeLoggined: true,
+          headerTitle: "SSL certificate",
+          isNeedBackButton: true,
+        },
+        component: () => import("../routes/ssl/ssl.vue"),
+      },
+      {
+        path: "cabinet",
+        name: "cabinet",
+        meta: {
+          mustBeLoggined: true,
+          footerTitle: "settings",
+          headerTitle: "settings",
+          isNeedBackButton: true,
+        },
+        component: () => import("../routes/userSettings.vue"),
+      },
+    ],
+  },
+  {
+    path: "/cloud/:uuid",
+    name: "openCloud_new",
+    component: () => import("../components/appMain/cloud/openCloud_new.vue"),
     meta: {
       mustBeLoggined: true,
     },
-    component: () => import('../components/appMain/invoice/openTransaction.vue')
-  }
-]
+  },
+  {
+    path: "/cloud/*",
+    component: () => import("../components/appMain/cloud/cloudRouter.vue"),
+    meta: {
+      mustBeLoggined: true,
+    },
+    children: [
+      {
+        path: "vnc",
+        name: "VNC",
+        component: () => import("../components/appMain/cloud/vnc.vue"),
+      },
+      {
+        path: "*",
+        redirect: (to) => `/cloud-${to.params.uuid}`,
+      },
+      {
+        path: "",
+        name: "openCloud",
+        component: () =>
+          import("../components/appMain/cloud/openCloud_new.vue"),
+      },
+    ],
+  },
+  {
+    path: "/ticket-*",
+    name: "ticket",
+    meta: {
+      mustBeLoggined: true,
+    },
+    component: () => import("../components/appMain/support/ticketchat.vue"),
+  },
+  {
+    path: "/invoice/:uuid",
+    name: "invoiceFS",
+    meta: {
+      mustBeLoggined: true,
+    },
+    component: () => import("../components/appMain/invoice/openInvoice.vue"),
+  },
+  {
+    path: "/transaction/:uuid",
+    name: "transaction",
+    meta: {
+      mustBeLoggined: true,
+    },
+    component: () =>
+      import("../components/appMain/invoice/openTransaction.vue"),
+  },
+  {
+    path: "/crm",
+    name: "crm",
+    meta: {
+      mustBeLoggined: true,
+    },
+    component: () => import("../routes/crm.vue"),
+  },
+];
 
 const router = new VueRouter({
-	// mode: 'history',
-	// base: process.env.BASE_URL,
-	routes
-})
+  // mode: 'history',
+  // base: process.env.BASE_URL,
+  routes,
+});
 
-
-export default router
+export default router;
